a81e5f0b89e479c894876040305e8c7300d96848
WunnaThaw <wunna.pe@gmail.com>
2023-05-21 20:34:19 +0630

    Abstract Factory Design Pattern

---
+++ Factory/Program.cs
@@ -0,0 +1,24 @@
+﻿using DesktopPeripherals;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AbstractFactory
+{
+
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //Client Code
+            IMachineFactory factory = new AverageBudgetMachine();// Or new LowBudgetMachine();
+            ComputerShop shop = new ComputerShop(factory);
+            shop.AssembleMachine();
+
+            // Wait for user input
+            Console.ReadKey();
+        }
+    }
+}
